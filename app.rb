405080b88564ed94fcdea9c8e6cbcd3574a9c3fe
Ayana Bando <ayanaflax@yahoo.com>
2018-10-11 16:48:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,13 @@ class App < Sinatra::Base
     set :session_secret, "secret" #sets encryption key
   end
 
+  get '/' do
+    erb :index
+  end
   
+ post '/checkout' do
+
+  end
   get '/hey' do 
     @session = session
   end
